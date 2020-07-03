5b5fd3b9c9e4dc0ce1fdc7c2eb199414a42c94e6
lilas-saleh <lilas.ahmad.saleh@gmail.com>
2020-07-03 11:51:46 +0300

    Updates

---
+++ precache-manifest.bc38eeb7e903c702f3eb6c5043568af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5041d86e09dd98ecfed69b00a1b12bb1",
+    "url": "/portfolio-template/index.html"
+  },
+  {
+    "revision": "a98c76b0810a57e636e7",
+    "url": "/portfolio-template/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "820f85bdd3b984531ebf",
+    "url": "/portfolio-template/static/js/2.c261b0a4.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio-template/static/js/2.c261b0a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98c76b0810a57e636e7",
+    "url": "/portfolio-template/static/js/main.846dbce4.chunk.js"
+  },
+  {
+    "revision": "00db7c972f24fda8e73c",
+    "url": "/portfolio-template/static/js/runtime-main.ccf1117e.js"
+  }
+]);
